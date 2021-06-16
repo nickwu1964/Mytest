857af81692d9827b119472ec9a4f4c8a5b165704
nickw <nickw@scosche.com>
2021-06-16 15:22:30 -0700

    first commit practice

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//print out to user
+	cout << "Hello World !!!" << endl;
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "message.h"
+#include <cstdlib>
+
+using namespace std;
+
+main()
+{
+	message m;
+	m.printMessage();
+
+	return 0;
+}
